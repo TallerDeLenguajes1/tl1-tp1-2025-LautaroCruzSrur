1b2815e5bf04e719747fa2a5be7749764954ea6c
Lautaro G Cruz Srur <105937766+LautaroCruzSrur@users.noreply.github.com>
2025-03-26 20:07:34 -0300

    ejecicio 2.4 completado

--- tp1_2.c
+++ tp1_2.c
@@ -1,6 +1,8 @@
 #include <stdio.h>
 
 void cuadrado (int var);
+void invertir (int a , int b);
+void orden (int a, int b);
 
 int main(){
     int num = 0;
@@ -9,6 +11,12 @@ int main(){
     scanf("%d", &num);
     cuadrado(num);
     printf("Contenido de la variable: %d y la direccion %p\n", num , *ptr);
+    printf("Ingrese dos valores numericos para invertir\n");
+    int a = 0;
+    int b = 0;
+    scanf("%d %d", &a, &b);
+    invertir(a, b);
+
 
 
 
@@ -19,3 +27,9 @@ void cuadrado (int var){
     int resultado = var * var;
     printf("El cuadrado de %d es %d\n", var, resultado);
 }
+void invertir (int a , int b){
+    int aux = a;
+    a = b;
+    b = aux;
+    printf("El valor de a es %d y el valor de b es %d\n", a, b);
+};
