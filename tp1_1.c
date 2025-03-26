2eb03ee0ca67de14f69d17dfc4a33f8185aa59df
Lautaro G Cruz Srur <105937766+LautaroCruzSrur@users.noreply.github.com>
2025-03-26 19:56:12 -0300

    desarollando hasta el punto 4

--- tp1_1.c
+++ tp1_1.c
@@ -1,7 +1,18 @@
 #include <stdio.h>
 
-int main(){
-    printf("Hola mundo\n");
+int main() {
+
+    printf("Hola mundo!\n");
+
+    int var = 10;       // Variable normal
+    int *ptr = &var;    // Puntero apuntando a var
+
+    printf("Contenido de la variable: %d\n", var);
+    printf("Direccion de la variable (&var): %p\n", &var);
+    printf("Contenido del puntero (*ptr): %d\n", *ptr);
+    printf("Direccion almacenada en el puntero (ptr): %p\n", *ptr);
+    printf("Direccion de memoria del puntero (&ptr): %p\n", *&ptr);
+    
 
     return 0;
 }
