1ed9d6a8ffcbd731d65fb770cb32cafe359a429a
Lautaro G Cruz Srur <105937766+LautaroCruzSrur@users.noreply.github.com>
2025-03-26 20:05:08 -0300

    punto c

--- tp1_2.c
+++ tp1_2.c
@@ -1,17 +1,21 @@
 #include <stdio.h>
 
-int cuadrado (int var);
+void cuadrado (int var);
 
 int main(){
     int num = 0;
+    int *ptr = &num; 
     printf("Ingrese un numero\n");
     scanf("%d", &num);
     cuadrado(num);
+    printf("Contenido de la variable: %d y la direccion %p\n", num , *ptr);
+
+
 
     return 0;
 }
 
-int cuadrado (int var){
+void cuadrado (int var){
     int resultado = var * var;
-    return printf("El cuadrado de %d es %d\n", var, resultado); ;
+    printf("El cuadrado de %d es %d\n", var, resultado);
 }
